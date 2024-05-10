814e55dc3c46e0f78bb2126efd2f186e98d18345
kiwibang <>
2024-05-10 11:18:48 +0800

    add stuckinatimeloop

---
+++ stuckinatimeloop.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++) {
+        cout << i << " Abracadabra" << endl;
+    }
+    return 0;
+}
