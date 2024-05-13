03018b7542dde50d5c6b883e3cb577b78a851303
kiwibang <>
2024-05-13 10:27:20 +0800

    add temperature

---
+++ temperature.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double x, y;
+    cin >> x >> y;
+    if (y == 1) {
+        if (x == 0) {
+            cout << "ALL GOOD" << endl;
+        } else {
+            cout << "IMPOSSIBLE" << endl;
+        }
+        return 0;
+    }
+    cout << - (x / (y - 1)) << endl;
+    return 0;
+}
