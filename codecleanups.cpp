e7c38a22086960f80637146141c3aa8b055b1c39
kiwibang <>
2024-05-23 16:45:26 +0800

    codecleanup codecleanups

--- codecleanups.cpp
+++ codecleanups.cpp
@@ -4,16 +4,17 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-    vector<bool> dirtyPushes(366, false);
+    vector<int> dirtyPushes;
     for (int i = 0; i < n; ++i) {
         // get the current dirty push
         int currDay;
         cin >> currDay;
-        dirtyPushes[currDay] = true;
+        dirtyPushes.push_back(currDay);
     }
     int res = 0;
     int currDirtiness = 0;
     int numDirty = 0;
+    int idx = 0;
     for (int j = 1; j <= 365; ++j) {
         currDirtiness += numDirty;
         if (currDirtiness >= 20) {
@@ -21,7 +22,8 @@ int main() {
             numDirty = 0;
             ++res;
         }
-        if (dirtyPushes[j]) {
+        if (dirtyPushes[idx] == j) {
+            ++idx;
             ++numDirty;
         }
     }
