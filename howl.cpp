ea280fbacd1f78afb23521698e30dc88cf7c73e9
kiwibang <>
2024-05-23 17:48:43 +0800

    add howl

---
+++ howl.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string awoo;
+    cin >> awoo;
+    cout << "AHOW";
+    for (int i = 4; i <= awoo.length() * 2; ++i) {
+        cout << 'O';
+    }
+    cout << endl;
+    return 0;
+}
