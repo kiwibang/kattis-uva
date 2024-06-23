9877fb28f6306a691d2915711513b78939317e1a
kiwibang <>
2024-06-23 17:00:35 +0800

    add spavanac

---
+++ spavanac.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int h, m;
+    cin >> h >> m;
+    m = m - 45;
+    if (m < 0) {
+        --h;
+        m += 60;
+        if (h < 0) {
+            h = 23;
+        }
+    }
+    cout << h << " " << m << endl; 
+    return 0;
+}
