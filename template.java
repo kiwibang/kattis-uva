977197c8873a127ff948aa0ac331dd5c85e6a8ef
kiwibang <>
2025-01-05 15:55:19 +0800

    add template for java solutions

---
+++ template.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class template {
+    static class FS {
+        static BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        static StringTokenizer st=new StringTokenizer("");
+
+        static String next() throws IOException {
+            while (!st.hasMoreTokens()) { st=new StringTokenizer(br.readLine()); }
+            return st.nextToken();
+        }
+
+        static int nextInt() throws IOException {
+            String out = next();
+            try {
+                return Integer.parseInt(out);
+            } catch (NumberFormatException e) {
+                throw new NumberFormatException("lol");
+            }
+        }
+
+        static Long nextLong() throws IOException {
+            String out = next();
+            try {
+                return Long.parseLong(out);
+            } catch (NumberFormatException e) {
+                throw new NumberFormatException("lol");
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+
+    }
+
+}
